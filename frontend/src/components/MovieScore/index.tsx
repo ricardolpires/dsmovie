3813ad75ad3f3e24d9a71b0f7ab427e53550bc0e
Ricardo Pires <ricardothx@gmail.com>
2022-01-16 20:44:22 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,19 +1,22 @@
-import MovieStars from "components/MovieStars";
-import './styles.css';
+import MovieStars from "Components/MovieStars";
+import "./styles.css";
 
-function MovieScore(){
+type Props = {
+  score: number;
+  count: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+const MovieScore = ( { score, count } : Props ) => {
 
-    return(
-        <div className="dsmovie-score-container">
-    <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-    <MovieStars />
-    <p className="dsmovie-score-count">{count} avaliações</p>
-</div>
+  return (
+    <div className="dm-movie_score_container">
+      <p className="dm-movie_score_value">
+        {score > 0 ? score.toFixed(1) : "-"}
+      </p>
+      <MovieStars score={score}/>
+      <p className="dm-movie_score_count">{count} avaliações</p>
+    </div>
+  );
+};
 
-    );
-    }
-
-    export default MovieScore;
+export default MovieScore;
