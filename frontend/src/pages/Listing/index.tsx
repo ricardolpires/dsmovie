ecd5e61842ccd6a450810362ae7004d64128ec13
Ricardo Pires <ricardothx@gmail.com>
2022-01-16 19:55:16 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,33 +1,52 @@
-import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination";
+import axios from "axios";
+import MovieCard from "Components/MovieCard";
+import Pagination from "Components/Pagination";
+import { useState, useEffect } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
-function Listing(){
-    return(
-        <>
-        
-        <Pagination/>
+const Listing = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
 
-        <div className="container">
-            <div className="row">
-<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-<MovieCard />
-    </div>
-    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-<MovieCard />
-    </div>
-    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-<MovieCard />
-    </div>
-    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-<MovieCard />
-    </div>
-    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-<MovieCard />
-    </div>
-     </div>
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((res) => {
+        const data = res.data as MoviePage;
+        setPage(data);
+      })
+      .catch();
+  }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber : number) => {
+    setPageNumber(newPageNumber);
+  }
+
+  return (
+    <>
+      <Pagination page={page} onChange={handlePageChange}/>
+      <div className="container">
+        <div className="row">
+          {page.content.map((movie) => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Listing;
