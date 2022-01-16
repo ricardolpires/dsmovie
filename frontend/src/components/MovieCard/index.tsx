51b2422d3fc0988826702c1891b324d572eb74a7
Ricardo Pires <ricardothx@gmail.com>
2022-01-16 20:17:34 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,32 +1,29 @@
-import MovieScore from "components/MovieScore";
+import MovieScore from "Components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard(){
-
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-
-    return (
-<div>
-    <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-    <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
-
-        <Link to={`/form/${movie.id}`}>
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </Link>
-        
-    </div>
-</div>
-
-    );
-
+type Props = {
+  movie: Movie;
 }
+
+const MovieCard = ( { movie } : Props ) => {
+
+  return (
+    <div>
+      <img
+        className="dm-movie_movie_card_image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dm-movie_card_bottom_container">
+        <h3>{movie.title}</h3>
+        <MovieScore count={movie.count} score={movie.score}/>
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dm-movie_btn">Avaliar</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
