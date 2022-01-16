49a057da49f0d60e3d4d43795fb0a595e841c695
Ricardo Pires <ricardothx@gmail.com>
2022-01-16 20:53:38 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,18 +1,34 @@
-import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
-import './styles.css';
-function Pagination(){
-    return (
-<div className="dsmovie-pagination-container">
-    <div className="dsmovie-pagination-box">
-        <button className="dsmovie-pagination-button" disabled={true} >
-            <Arrow />
+import { ReactComponent as Arrow } from "assets/img/arrow.svg";
+import { MoviePage } from "types/movie";
+import "./styles.css";
+
+type Props = {
+  page: MoviePage;
+  onChange: Function;
+};
+
+const Pagination = ({ page, onChange }: Props) => {
+  return (
+    <div className="dm-movie_pagination_container">
+      <div className="dm-movie_pagination_box">
+        <button
+          className="dm-movie_pagination_button"
+          disabled={page.first}
+          onClick={() => onChange(page.number - 1)}
+        >
+          <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="dsmovie-pagination-button" disabled={false} >
-            <Arrow className="dsmovie-flip-horizontal" />
+        <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+        <button 
+        className="dm-movie_pagination_button" 
+        disabled={page.last}
+        onClick={() => onChange(page.number + 1)}
+        >
+          <Arrow className="dm-movie_flip_horizontal" />
         </button>
+      </div>
     </div>
-</div>
-    );
-}
+  );
+};
+
 export default Pagination;
